4db880affb96463c4a51aa20e8cd03565aa93f8e
Raphael Magalhães <raphael.sdm@gmail.com>
2019-12-29 18:10:08 -0300

    Add more requests to ClubhouseRepository

--- src/repositories/ClubHouseRepository.js
+++ src/repositories/ClubHouseRepository.js
@@ -1,36 +1,96 @@
 const http = require('../infrastructure/HTTP')
 
-const getStoryHistoryByIdConfig = storyId => ({
-  headers: {
-    authority: 'app.clubhouse.io',
-    pragma: 'no-cache',
-    'cache-control': 'no-cache',
-    'clubhouse-organization': process.env.CLUBHOUSE_ORGANIZATION,
-    'clubhouse-page': 'story',
-    'content-type': 'application/json; charset=utf-8',
-    accept: '*/*',
-    'x-requested-with': 'XMLHttpRequest',
-    'clubhouse-company': process.env.CLUBHOUSE_COMPANY,
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'cors',
-    referer: `https://app.clubhouse.io/${process.env.CLUBHOUSE_ORGANIZATION_SLUG}/story/${storyId}`,
-    cookie: `clubhouse-session=${process.env.CLUBHOUSE_USER_SESSION_TOKEN};`
-  },
-  uri: `https://app.clubhouse.io/backend/api/private/stories/${storyId}/history`,
-  method: 'GET'
-})
+const CLUBHOUSE_API_TOKEN = process.env.CLUBHOUSE_API_TOKEN
 
-const getStoryHistoryById = async id => {
-  const config = getStoryHistoryByIdConfig(id)
-  const callback = (error, response, body) => body
+const CLUBHOUSE_ORGANIZATION = process.env.CLUBHOUSE_ORGANIZATION
+const CLUBHOUSE_COMPANY = process.env.CLUBHOUSE_COMPANY
+const CLUBHOUSE_ORGANIZATION_SLUG = process.env.CLUBHOUSE_ORGANIZATION_SLUG
+const CLUBHOUSE_USER_SESSION_TOKEN = process.env.CLUBHOUSE_USER_SESSION_TOKEN
 
-  const response = await http.call(config, callback)
+const doCall = async config => await http.call(config, async (error, response, body) => body)
 
-  return JSON.parse(response)
+const getProjects = async () => {
+  const config = {
+    url: 'https://api.clubhouse.io/api/beta/projects?token=' + CLUBHOUSE_API_TOKEN,
+    headers: {
+      'content-type': 'application/json; charset=utf-8'
+    },
+    json: true
+  }
+
+  return await doCall(config)
 }
 
-const ClubHouseRepository = {
+const getWorkflows = async () => {
+  const config = {
+    url: 'https://api.clubhouse.io/api/v3/workflows?token=' + CLUBHOUSE_API_TOKEN,
+    headers: {
+      'content-type': 'application/json; charset=utf-8'
+    },
+    method: 'GET',
+    json: true
+  }
+
+  return await doCall(config)
+}
+
+const getIterations = async () => {
+  const config = {
+    url: 'https://api.clubhouse.io/api/v3/iterations?token=' + CLUBHOUSE_API_TOKEN,
+    headers: {
+      'content-type': 'application/json; charset=utf-8'
+    },
+    method: 'GET',
+    json: true
+  }
+
+  return await doCall(config)
+}
+
+const getStoriesByProjectId = async id => {
+  const config = {
+    url: 'https://api.clubhouse.io/api/beta/stories/search?token=' + CLUBHOUSE_API_TOKEN,
+    headers: {
+      'content-type': 'application/json; charset=utf-8'
+    },
+    method: 'POST',
+    json: true,
+    body: { project_ids: [id] }
+  }
+
+  return await doCall(config)
+}
+
+const getStoryHistoryById = async (id, name) => {
+  const config = {
+    headers: {
+      authority: 'app.clubhouse.io',
+      pragma: 'no-cache',
+      'cache-control': 'no-cache',
+      'clubhouse-organization': CLUBHOUSE_ORGANIZATION,
+      'clubhouse-page': 'story',
+      'content-type': 'application/json; charset=utf-8',
+      accept: '*/*',
+      'x-requested-with': 'XMLHttpRequest',
+      'clubhouse-company': CLUBHOUSE_COMPANY,
+      'sec-fetch-site': 'same-origin',
+      'sec-fetch-mode': 'cors',
+      referer: `https://app.clubhouse.io/${CLUBHOUSE_ORGANIZATION_SLUG}/story/${id}`,
+      cookie: `clubhouse-session=${CLUBHOUSE_USER_SESSION_TOKEN};`
+    },
+    uri: `https://app.clubhouse.io/backend/api/private/stories/${id}/history`,
+    method: 'GET'
+  }
+
+  return await doCall(config)
+}
+
+const ClubhouseRepository = {
+  getProjects,
+  getWorkflows,
+  getIterations,
+  getStoriesByProjectId,
   getStoryHistoryById
 }
 
-module.exports = ClubHouseRepository
+module.exports = ClubhouseRepository
