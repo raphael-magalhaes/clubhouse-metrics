c97300687d687da2ed6f3eccfe8fc5e9a88b979b
Raphael Magalhães <raphael.sdm@gmail.com>
2019-12-29 16:43:38 -0300

    Add saveAsCSV and readFile to FileSystem

--- src/infrastructure/FileSystem.js
+++ src/infrastructure/FileSystem.js
@@ -3,7 +3,14 @@ const csvStringify = require('csv-stringify')
 
 const FileSystem = {
   saveAsJSON: (data, path) => fs.writeFileSync(path, JSON.stringify(data, null, 4)),
-  saveAsCSV: (data, path, config) => csvStringify(data, config, (error, output) => fs.writeFileSync(path, output))
+  saveAsCSV: (data, path, config) => csvStringify(data, config, (error, output) => fs.writeFileSync(path, output)),
+  readFile: path => {
+    try {
+      return JSON.parse(fs.readFileSync(path))
+    } catch (error) {
+      return undefined
+    }
+  }
 }
 
 module.exports = FileSystem
